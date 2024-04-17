fc85b1275624217163c9381bfe6482286f1c6494
Samson Eze <38159339+iamemmy@users.noreply.github.com>
2024-04-17 12:43:05 +0100

    Debugged quotes
    Debugged quotes

--- components/ReviewSlider.jsx
+++ components/ReviewSlider.jsx
@@ -34,7 +34,7 @@ const ReviewsSlider = ({ reviews }) => {
     <Slider {...settings}>
       {reviews.map((review, index) => (
         <div key={index} className="review text-white py-4 px-4 border border-gray-600">
-          <p className="review-text text-white mb-4">"{review.text}"</p>
+          <p className="review-text text-white mb-4">&quot;{review.text}&quot;</p>
           <p className="review-author text-gray-300">- {review.author} (<span className='text-yellow-500 italic'>{review.role}</span>)</p>
         </div>
       ))}
